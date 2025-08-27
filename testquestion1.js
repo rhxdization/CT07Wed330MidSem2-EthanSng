cdcbc8d35496d30c7ac18afbc18a1b1ed3388401
Acu <wongethan120@gmail.com>
2025-08-27 08:08:13 +0000

    cm

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,13 @@
+function setup() {
+    new Canvas(600,400);
+    background(255);
+    world.gravity.y = 10;
+}
 
+function draw() {
+    background(255);
+    if (mouse.presses("left")) {
+        let x = new Sprite (mouseX, mouseY, 50, 50);
+        x.collider = ("dynamic");  
+    }
+}
