fe495225509a8f00cb701941095819d54cbee250
Acu <wongethan120@gmail.com>
2025-08-27 08:22:16 +0000

    fin

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,20 @@
+function setup() {
+    new Canvas(600,400);
+    background(255);
+    world.gravity.y = 10;
+    
+    ground = new Sprite();
+    ground.collider = "static";
+    ground.y = 400;
+    ground.height = 30;
+    ground.width = 600;
+}
 
+function draw() {
+    background(255);
+    if (mouse.presses("left")) {
+        let x = new Sprite (mouseX, mouseY, 50, 50);
+        x.collider = ("dynamic");
+        x.bounciness = 0.6;  
+    }
+}
